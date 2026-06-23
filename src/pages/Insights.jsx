4c0d6837aa0bc8f8f598d6ca90cd1f750ca1f9fb
Anthony Klimas <anthonyklimas7@gmail.com>
2026-06-23 17:49:48 -0400

    fixed insights format for other devices

--- src/pages/Insights.jsx
+++ src/pages/Insights.jsx
@@ -51,7 +51,7 @@ function Insights()
 
     </motion.div>
 
-    <motion.div
+    <motion.div className= "motionWrapper"
         initial = {{opacity: 0, x: 200}}
         animate = {{opacity: 1, x: 0}}
         exit ={{opacity: 0, x: -200}}
